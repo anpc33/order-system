002fdaa9a523eacffa4588d4f9aaa2753cd6f9c9
anpc33 <ancqph51578@gmail.com>
2025-06-13 20:04:47 +0700

    Resolve conflict

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Migrate & Seed
+# Migrate & Seed (nếu cần)
 php artisan migrate --force
 php artisan db:seed --force
 
-# Start Queue Worker (in background)
+# Start Queue Worker (background)
 php artisan queue:work --tries=3 &
 
-# Serve Laravel App
+# Serve Laravel App (listen 0.0.0.0:8000)
 php artisan serve --host=0.0.0.0 --port=8000
