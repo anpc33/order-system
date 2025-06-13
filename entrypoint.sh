36b09d0eb97d36d61d6cdaafb7fc832fa9b7cdde
Cao Quoc An <155868386+anpc33@users.noreply.github.com>
2025-06-14 00:11:38 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,4 @@ php artisan db:seed --force
 php artisan queue:work --tries=3 &
 
 # Serve Laravel App (listen 0.0.0.0:8000)
-php artisan serve --host=0.0.0.0 --port=8000
+php artisan serve --host=0.0.0.0 --port=8000 --public=public
